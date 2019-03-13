1a25d0bce71075da6cd992ed26afb12b68f2d205
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-13 17:00:34 -0500

    added state with one boolean prop to dislay certain parts of the bio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+   constructor() {
+      super(); 
+      this.state = {
+         displayBio: false
+      };
+   }
+   
+   render () {
+      return (
+         <div>
+            <h1>Hello!</h1>
+            <p>My name is Julie. I am a web developer. </p>
+            <p>I'm always looking for meaningful project work.</p>
+            { 
+               this.state.displayBio ? ( 
+                  <div>
+                     <p>I live in the Twin Cities and love to walk the trails and paths in my neighborhood.</p>
+                     <p>My favorite JavaScript library is React.</p>
+                     <p>In addition to coding, I love a little true crime TV and coffee!</p>  
+                  </div> 
+               ) : null
+            }
+         </div>
+      );
+   }
 }
 
 export default App;
+
