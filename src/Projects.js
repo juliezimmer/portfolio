26cad79c7904a2145b318ecba6f5f61a55d14208
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-14 12:38:59 -0500

    added Project class component that displays project work

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+   // this Project component is using JSX to display what was originally in the .map() function in the Projects component below.
+   render(){
+      console.log(this.props);
+
+      const { title, image, description, link } = this.props.project;
+      
+      return (
+         <div style={{ display: 'Inline-block', width: 300, margin: 10 }}>
+            <h3>{title}</h3>
+            <img src={image} alt='profile' style={{ width: 200, height: 120 }}/>
+            <p>{description}</p>
+            <a href={link}>{link}</a>
+
+         </div>
+      );
+   }
+}
+
 class Projects extends Component {
    render(){
       return (
@@ -10,9 +29,10 @@ class Projects extends Component {
                {
                   PROJECTS.map((PROJECT) => {
                      return ( 
-                        <div key={PROJECT.id}>
-                           {PROJECT.title} 
-                        </div>  
+                        <Project 
+                           key={PROJECT.id} 
+                           project={PROJECT} 
+                        />  
                      );   
                   })
                }
